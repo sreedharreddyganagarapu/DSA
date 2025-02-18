22c16ab401b1606c5deb4e71aff30a5a4187e1d2
sreedharreddyganagarapu <gangarapusreedharreddy@gmail.com>
2025-02-18 22:06:13 +0530

    Update ScoreoftheString.java

--- Strings/ScoreoftheString.java
+++ Strings/ScoreoftheString.java
@@ -1,7 +1,5 @@
 //You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
 
-
-
 import java.util.*;
 public class ScoreoftheString {
     public static int Ascii(String s) {
